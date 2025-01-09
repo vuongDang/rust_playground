f8270218e86a3b6117cddcdc126ee1218c619b7f
vuongDang <vuongdang9@gmail.com>
2025-01-09 01:45:45 +0100

    specialization: autoderef with value instead of ref

--- specialization/src/autoderef.rs
+++ specialization/src/autoderef.rs
@@ -1,45 +1,88 @@
 //! Autoderef for specialization
 //! https://lukaskalbertodt.github.io/2019/12/05/generalized-autoref-based-specialization.html
+//!
+//! We implement autoderef with a tag system so that a value can be passed instead of just a ref
 
+#![allow(dead_code)]
 use std::fmt::{Debug, Display};
 
-struct Wrap<T>(T);
+// A custom type which are used as input
+// We use a custom type to avoid interference
+// with other blanket implementation such as Display for String
+struct InputType<T>(T);
 
+// The resulting type we're trying to convert to using specialization
+// from different `InputType`
+struct OutputType(String);
+
+struct TagString;
+struct TagDisplay;
+struct TagDebug;
+
+// Most specific impl
 trait WithString {
-    fn foo(&self); 
+    fn tag(&self) -> TagString;
 }
 
-impl WithString for &&Wrap<String> { 
-    fn foo(&self) { println!("with String") }
-}
-
-trait WithDebug {
-    fn foo(&self) ;
-}
-
-impl<T: Debug> WithDebug for Wrap<T> { 
-    fn foo(&self) { println!("with Debug") }
-}
-
-trait WithDisplay {
-    fn foo(&self); 
-}
-
-impl<T: Display> WithDisplay for &Wrap<T>{
-    fn foo(&self) { println!("with Display") }
-}
-
-
-macro_rules! foo {
-    ($e:expr) => {
-        (&&&$e).foo()
+impl WithString for &&InputType<String> {
+    fn tag(&self) -> TagString {
+        TagString
     }
 }
 
+impl TagString {
+    fn create(self, input: InputType<String>) -> OutputType {
+        println!("with string");
+        OutputType(input.0)
+    }
+}
+
+// 2nd specific impl
+trait WithDisplay {
+    fn tag(&self) -> TagDisplay;
+}
+
+impl<T: Display> WithDisplay for &InputType<T> {
+    fn tag(&self) -> TagDisplay {
+        TagDisplay
+    }
+}
+
+impl TagDisplay {
+    fn create<T: Display>(self, input: InputType<T>) -> OutputType {
+        println!("with display");
+        OutputType(format!("{}", input.0))
+    }
+}
+
+// Most generic impl
+trait WithDebug {
+    fn tag(&self) -> TagDebug;
+}
+
+impl<T: Debug> WithDebug for InputType<T> {
+    fn tag(&self) -> TagDebug {
+        TagDebug
+    }
+}
+
+impl TagDebug {
+    fn create<T: Debug>(self, input: InputType<T>) -> OutputType {
+        println!("with debug");
+        OutputType(format!("{:?}", input.0))
+    }
+}
+
+macro_rules! foo {
+    ($e:expr) => {
+        (&&&$e).tag().create($e)
+    };
+}
+
 pub fn main() {
-    let w1 = Wrap(String::from("toto"));
-    let w2 = Wrap(3);
-    let w3 = Wrap(["a", "b"]);
+    let w1 = InputType(String::from("toto"));
+    let w2 = InputType(3);
+    let w3 = InputType(["a", "b"]);
 
     foo!(w1);
     foo!(w2);
