3130e11ea4e9185568875b0d86dda2b68954ffe7
vuongDang <vuongdang9@gmail.com>
2025-03-28 00:14:09 +0100

    microservices: implement client and health-check

--- bootcamp/microservices/src/auth-service/main.rs
+++ bootcamp/microservices/src/auth-service/main.rs
@@ -15,8 +15,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Port 50051 is the recommended gRPC port.
     let addr = "[::0]:50051".parse()?;
 
-    let users_service: Box<Mutex<dyn Users + Send + Sync + 'static>> = todo!(); // Create user service instance
-    let sessions_service: Box<Mutex<dyn Sessions + Send + Sync + 'static>> = todo!(); //Create session service instance
+    let users_service: Box<Mutex<dyn Users + Send + Sync + 'static>> =
+        Box::new(Mutex::new(UsersImpl::default()));
+    let sessions_service: Box<Mutex<dyn Sessions + Send + Sync + 'static>> =
+        Box::new(Mutex::new(SessionsImpl::default()));
 
     let auth_service = AuthService::new(users_service, sessions_service);
 
